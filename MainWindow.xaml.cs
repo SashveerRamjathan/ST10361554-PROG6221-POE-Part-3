8cd33ad72cff1e4eeaaba165224993ec7a79fea8
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-06-17 22:08:30 +0200

    Designed startup window in XAML and coded up timer for the window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ST10361554_PROG6221_POE.Images;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ST10361554_PROG6221_POE
 {
@@ -20,9 +22,24 @@ namespace ST10361554_PROG6221_POE
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Code Attribution for Timer
+        private DispatcherTimer _timer;
         public MainWindow()
         {
             InitializeComponent();
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(3); // 3 seconds
+            _timer.Tick += (sender, e) => 
+            {
+                MenuWindow menuWindow = new MenuWindow();
+                menuWindow.Show();
+                this.Close();
+            };
+            _timer.Start();
+
+            this.Show();
+
         }
     }
 }

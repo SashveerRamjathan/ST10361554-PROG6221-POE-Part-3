03769756d9b8f51cefb7e62036178f1b112b9250
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-06-25 22:16:29 +0200

    Changed App icon, added events and mapped events to menu buttons. Added windows for menu options

--- MenuWindow.xaml.cs
+++ MenuWindow.xaml.cs
@@ -26,27 +26,35 @@ namespace ST10361554_PROG6221_POE.Images
 
 		private void CreateRecipeBtn_Click(object sender, RoutedEventArgs e)
 		{
-
+			CreateRecipeWindow createRecipeWindow = new CreateRecipeWindow();
+			createRecipeWindow.Show();
+			this.Close();
 		}
 
 		private void ViewRecipeBookBtn_Click(object sender, RoutedEventArgs e)
 		{
-
+			ViewRecipeBookWindow viewRecipeBookWindow = new ViewRecipeBookWindow();
+			viewRecipeBookWindow.Show();
+			this.Close();
 		}
 
 		private void CreateMenuBtn_Click(object sender, RoutedEventArgs e)
 		{
-
+			CreateMenuWindow createMenuWindow = new CreateMenuWindow();
+			createMenuWindow.Show();
+			this.Close();
 		}
 
 		private void ViewFoodGroupInfoBtn_Click(object sender, RoutedEventArgs e)
 		{
-
+			ViewFoodGroupInfoWindow viewFoodGroupInfoWindow = new ViewFoodGroupInfoWindow();
+			viewFoodGroupInfoWindow.Show();
+			this.Close();
 		}
 
 		private void CloseAppBtn_Click(object sender, RoutedEventArgs e)
 		{
-
+			this.Close();
 		}
 	}
 }

f7a6d8829aa26f1b638057678b995008310369e5
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-06-26 20:38:22 +0200

    Added Recipe Methods Class

---
+++ RecipeMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10361554_PROG6221_POE
+{
+	public class RecipeMethods
+	{
+
+		// List collection to store more than one recipe
+		public List<Recipe> recipes = new List<Recipe>();
+
+		// Instance of FoodGroupInformation to access food group information
+		FoodGroupInformation FoodGroupInformation = new FoodGroupInformation();
+
+		// Instance of CalorieCounter to perform calorie-related operations
+		CalorieCounter counter = new CalorieCounter();
+	}
+}

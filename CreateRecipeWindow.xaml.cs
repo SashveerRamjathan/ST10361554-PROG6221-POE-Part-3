0aede17f061a3b7af3e75f3d90d90ba853df1ae0
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-06-26 20:24:26 +0200

    Added Recipe Step Class

--- CreateRecipeWindow.xaml.cs
+++ CreateRecipeWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ST10361554_PROG6221_POE.Images;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,45 @@ namespace ST10361554_PROG6221_POE
 		public CreateRecipeWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void CreateRecipeBtn_Click(object sender, RoutedEventArgs e)
+		{
+			string recipeName = RecipeNameTextbox.Text;
+			int numSteps = (int)NumberStepsSlider.Value;
+			int numIngredients = (int)NumberIngredientsSlider.Value;
+
+			if (recipeName == "")
+			{
+				MessageBox.Show("Please enter a recipe name.", "Enter a Recipe Name", MessageBoxButton.OK, MessageBoxImage.Warning);
+				return;
+			}
+
+
+
+		}
+
+		private void BackToMenuBtn_Click(object sender, RoutedEventArgs e)
+		{
+			MenuWindow menuWindow = new MenuWindow();
+			menuWindow.Show();
+			this.Close();
+		}
+
+		private void NumberStepsSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			if (NumStepsLbl != null)
+			{
+				NumStepsLbl.Content = $"Number Of Steps: {NumberStepsSlider.Value.ToString()}";
+			}
+		}
+
+		private void NumberIngredientsSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			if (NumIngredientsLbl != null)
+			{
+				NumIngredientsLbl.Content = $"Number Of Ingredients: {NumberIngredientsSlider.Value.ToString()}";
+			}
 		}
 	}
 }
---
+++ RecipeStep.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10361554_PROG6221_POE
+{
+	public class RecipeStep
+	{
+		public string? StepDescription { get; set; } // stores a description of each recipe step
+	}
+}

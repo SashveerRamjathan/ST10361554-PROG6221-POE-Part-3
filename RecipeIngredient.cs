7ec7073c4173d3c3e4fc57b8a5a134fcdf05bf40
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-06-26 20:26:04 +0200

    Added Recipe Ingredient Class

---
+++ RecipeIngredient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10361554_PROG6221_POE
+{
+	public class RecipeIngredient
+	{
+		// Property to store the ingredient's name
+		public string IngredientName { get; set; }
+
+		// Property to store the quantity of the ingredient added
+		public double IngredientQuantity { get; set; }
+
+		// Property to store the unit of measurement of the ingredient (e.g., cup, gram)
+		public string UnitOfMeasurement { get; set; }
+
+		// Property to store the food group to which the ingredient belongs
+		public FoodGroups Group { get; set; }
+
+		// Property to store the calories of the ingredient
+		public double Calories { get; set; }
+	}
+}
